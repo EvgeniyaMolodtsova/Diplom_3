a442cd9b61ba2e6a32c43eb3fe244794986d01e9
Evgeniya Molodtsova <molodtsova_95@mail.ru>
2023-02-18 22:23:44 +0100

    add test for Sing In

---
+++ src/test/java/TestRegistration.java
@@ -0,0 +1,67 @@
+import io.restassured.response.ValidatableResponse;
+import org.example.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+public class TestRegistration {
+
+    private WebDriver driver;
+
+    private String token;
+    UserClient userClient = new UserClient();
+
+    public void registration(){
+        driver.get("https://stellarburgers.nomoreparties.site/register");
+
+        SignUp signUp = new SignUp(driver);
+        signUp.waitFor();
+        User user = User.generate();
+        signUp.fillRegistrationForm(user);
+
+        ValidatableResponse login = userClient.login(User.getLoginFrom(user));
+        token = login.extract().path("accessToken");
+        Assert.assertEquals(200, login.extract().statusCode());
+    }
+
+    public void registrationWithShortPassword(){
+        driver.get("https://stellarburgers.nomoreparties.site/register");
+
+        SignUp signUp = new SignUp(driver);
+        signUp.waitFor();
+        User user = User.generate();
+        user.setPassword(Random.getRandom(5));
+        signUp.fillRegistrationForm(user);
+        Assert.assertEquals("Некорректный пароль", signUp.checkErrorMessage());
+    }
+
+    @After
+    public void cleanUp(){
+        if (token != null) {
+            userClient.delete(token);
+        }
+        driver.quit();
+    }
+
+    @Test
+    public void registrationChrome(){
+        driver = Drivers.getChromeDriver();
+
+        registration();
+    }
+
+    @Test
+    public void registrationYandex(){
+        driver = Drivers.getYandexDriver();
+
+        registration();
+    }
+
+    @Test
+    public void registrationChromeWithShortPassword() {
+        driver = Drivers.getChromeDriver();
+
+        registrationWithShortPassword();
+    }
+}
